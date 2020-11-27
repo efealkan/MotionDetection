b30b2b58a50c709463889883868c150c821f1982
Efe <e.alkan@student.tudelft.nl>
2020-11-27 16:50:18 +0100

    Basic cluster

--- motion_detector.py
+++ motion_detector.py
@@ -6,8 +6,38 @@ video_file = "input/test1.mp4"
 video = cv.VideoCapture(0)
 time.sleep(2.0)
 
+min_distance = 100  # min distance needed to form cluster
 min_area = 5000
-first_frame = None  # Is our background model
+first_frame = None
+
+
+def distance(p1, p2):
+    return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+
+
+def find_centroids(points):
+    clusters = []
+    dist = min_distance * min_distance
+    visited = [False] * len(points)
+    for i in range(len(points)):
+        if visited[i]:
+            continue
+        count = 1
+        point = [points[i][0], points[i][1]]
+        visited[i] = True
+        for j in range(i+1, len(points)):
+            if distance(points[i], points[j]) < dist:
+                point[0] += points[j][0]
+                point[1] += points[j][1]
+                count += 1
+                visited[j] = True
+        # if count == 1:
+        #     continue
+        point[0] /= count
+        point[1] /= count
+        clusters.append((point[0], point[1]))
+    return clusters
+
 
 while True:
     ret, frame = video.read()
@@ -32,7 +62,7 @@ while True:
     contours = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
 
-    pos = []
+    centres = []
 
     for c in contours:
         if cv.contourArea(c) < min_area:
@@ -43,8 +73,17 @@ while True:
         centre_x = int(x + (w/2))
         centre_y = int(y + (h/2))
 
+        centres.append((centre_x, centre_y))
+
         cv.circle(frame, (centre_x, centre_y), 4, (0, 0, 255), -1)
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    centroids = find_centroids(centres)
+
+    for centroid in centroids:
+        centre_x = int(centroid[0])
+        centre_y = int(centroid[1])
+        cv.circle(frame, (centre_x, centre_y), 4, (255, 0, 0), -1)
 
     cv.imshow("Frame", frame)
     cv.imshow("Whitened", thresh)
